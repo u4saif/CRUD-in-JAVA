48a470de6e89ca4fc5df813e6a39d22dededa9cf
eysaif <u4saif@gmail.com>
2024-02-20 17:54:11 +0530

    Implemented Authentication And Autherization of user with Database

--- src/main/java/com/employee/security/EmployeeSecurity.java
+++ src/main/java/com/employee/security/EmployeeSecurity.java
@@ -6,31 +6,22 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class EmployeeSecurity {
-
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-
-        UserDetails saif = User.builder()
-                .username("saif")
-                .password("{noop}ey123")
-                .roles("ADMIN","EMPLOYEE")
-                .build();
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}ey123")
-                .roles("EMPLOYEE")
-                .build();
-
-        return new InMemoryUserDetailsManager(saif,john);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -51,4 +42,23 @@ public class EmployeeSecurity {
 
         return http.build();
     }
+
+//*********In Memory User creation
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails saif = User.builder()
+//                .username("saif")
+//                .password("{noop}ey123")
+//                .roles("ADMIN","EMPLOYEE")
+//                .build();
+//
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}ey123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(saif,john);
+//    }
 }
